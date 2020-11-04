2223ae1a075a6dcbf9f24355bed9abe21e40d96f
Debebe Adewo <cm1dsa@sheffield.ac.uk>
2020-11-04 12:39:49 +0000

    Initial commit parameter values

---
+++ priorsRevised_new.R
@@ -0,0 +1,34 @@
+
+
+
+priors <- list(
+  psi_a = 77.9,
+  psi_b= 20.7,
+  recov_m =-1.677488,
+  recov_s = 0.1830687,
+  stab_m =0.6200967,
+  stab_s =0.06877732, 
+  fast_m = -2.368396,
+  fast_s = 0.3221463,
+  react_m = -6.886404,
+  react_s = 0.5751487,
+  relapse_m = -3.95, 
+  relapse_s=0.27,
+  mutb_m = -1.576473,
+  mutb_s=0.08773353,
+  cdr_m=5.6,
+  cdr_s=3.5,
+  beta_m=1.678, 
+  beta_s=0.371,
+  rho_a=-0.57, 
+  rho_b=0.085, 
+  rrprog_a=-0.298,
+  rrprog_b= 0.8104, 
+  rrcdr_a=-0.298,
+  rrcdr_b==0.2026      
+)
+
+
+
+
+
